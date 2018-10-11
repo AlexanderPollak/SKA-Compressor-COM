ca6e23847b6a03ec5bbb327b68cd9142208d97bb
Alexander.Pollak@physics.ox.ac.uk <Alexander.Pollak@physics.ox.ac.uk>
2018-10-11 14:43:37 +0100

    Creates setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SKA-Compressor-COM',
+    version='0.0.1',
+    description='python package that communicates with the EMSS Helium Compressor Controller',
+    license='MIT',
+    packages=['communication'],
+    author='Dr. Alexander Pollak',
+    author_email='Alexander.Pollak.87@gmail.com',
+    keywords=['SKA','HCC','EMSS'],
+    url='https://github.com/AlexanderPollak/SKA-Compressor-COM'
+)
